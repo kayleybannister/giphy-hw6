67dce6119488937c939852c1761d8c8d19d728e0
Kayley Bannister <kayley.bannister@gmail.com>
2019-07-08 13:40:28 -0400

    moved around some variables for testing, nothing working yet

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -6,15 +6,29 @@ var topics = ["golden retriever", "beach", "georgia", "dolly parton", "boats"];
 
 var userSearch = "";
 
-var apiKey = "U0Q0VCprObfzUgrS9wtYTFyYLlX0NZL2";
+//creating click event to start entire app
+$("button").on("click", function() {
 
-var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + topics || userSearch + "&rating=g&api_key=" + apiKey + "&limit=5";
+    console.log("Button clicked");
+
+//for whichever topic button the user clicks
+topic = $(this).attr("topic")
+
+var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&rating=g&api_key=U0Q0VCprObfzUgrS9wtYTFyYLlX0NZL2&limit=5";
 
 //calling AJAX
 $.ajax({
-    url: queryURL,
+    url: queryUrl,
     method: "GET"
   }).then(function(response) {
-      console.log(response);
 
+      console.log(response.url);
+
+    });
+
+
+
+
+
+// ends entire function
 });
